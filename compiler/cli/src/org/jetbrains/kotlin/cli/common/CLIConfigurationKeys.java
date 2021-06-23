b93b23d544785399a0102885ca516f155a536dbc
Jiaxiang Chen <jiaxiang@google.com>
2021-06-23 13:17:49 -0700

    expose original message collector in CLIConfiguration

--- compiler/cli/src/org/jetbrains/kotlin/cli/common/CLIConfigurationKeys.java
+++ compiler/cli/src/org/jetbrains/kotlin/cli/common/CLIConfigurationKeys.java
@@ -31,6 +31,10 @@ public class CLIConfigurationKeys {
 
     public static final CompilerConfigurationKey<MessageCollector> MESSAGE_COLLECTOR_KEY =
             CompilerConfigurationKey.create("message collector");
+
+    public static final CompilerConfigurationKey<MessageCollector> ORIGINAL_MESSAGE_COLLECTOR_KEY =
+            CompilerConfigurationKey.create("original message collector");
+
     public static final CompilerConfigurationKey<Boolean> ALLOW_KOTLIN_PACKAGE =
             CompilerConfigurationKey.create("allow kotlin package");
     public static final CompilerConfigurationKey<CommonCompilerPerformanceManager> PERF_MANAGER =
